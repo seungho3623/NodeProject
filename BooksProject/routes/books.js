dd2c5c5863258b102e71fc6f68350fedf3124501
Seungho Song <seungho3623@naver.com>
2023-09-09 17:42:17 +0900

    Week2 - BooksProject

--- BooksProject/routes/books.js
+++ BooksProject/routes/books.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* 도서검색 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '도서검색', pageName:'books/list.ejs' });
+  res.render('index', { title: '도서검색', pageName:'books/search.ejs' });
 });
 
 module.exports = router;
